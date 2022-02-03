10c6ccc44013f41b54504d441e53c5f1532fd654
Cole Webster <Cole.Webster@gmail.com>
2022-02-03 08:31:03 -0500

    Added prompts to index file

--- index.js
+++ index.js
@@ -3,33 +3,58 @@ const db = require('./db');
 
 //inqurier here
 function mainMenu() {
-    prompt([
+    .prompt([
         {
-            type: 'list',
             name: 'choice',
+            type: 'list',
             message: 'What would you like to do?'
             choices: [
                 {
-                    name: "View all employees"
+                    name: "View all employees",
                     value: "VIEW_EMPLOYEES"
                 },
                 // Employees by Department
+                {
+                    name: "View Employees by department",
+                    value: "Employees_Department"
+                },
                 // Employees by Manager
+                {
+                    name: "View employees by Manager",
+                    value: "Manager_ID"
+                },
                 //Add Employer
+                {
+                    name: "Select here to Add an Employee",
+                    value: "Add_Employee"
+                },
                 //Delete Employer
+                {
+                    name: "Select here to Delete an Employer",
+                    value: "Destroy_Employer"
+                },
                 //View all roles
+                {
+                    name: "View all roles",
+                    value: "View_All"
+                },
                 //More choices are needed!
                 {
-                    name: "Quit"
+                    name: "Add more choices?",
+                    value: "Add_More"
+                },
+                // Terminate the program
+                {
+                    name: "Quit",
                     value: "QUIT"
-                }
+                },
             ]
         }
-    ]).then(res => {
-        let choice = res.choices;
-        //Call the appropraite function on what the user chooses
-        //How would we organize this?
-    })
-}
+    // ]).then(res => {
+    //     let choice = res.choices;
+    //     Call the appropraite function on what the user chooses
+    //     How would we organize this?
+    //})
+};
 
 mainMenu();
