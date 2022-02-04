14d1ab12d2b6368bf60b2fc96d58836e6c11dbc2
Cole Webster <Cole.Webster@gmail.com>
2022-02-03 20:06:16 -0500

    Updating with functional node prompts

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
-
+const inquirer = require('inquirer');
+const {prompt} = require('inquirer');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -27,58 +28,26 @@ connection.connect(function (err) {
     if (err) throw err;
 });
 
-function mainMenu() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'View',
-            message: 'What would you like to do?'
-            choices: [
+mainMenu = () => {
+    inquirer
+        .prompt([
                 {
-                    name: "View all employees",
-                    value: "VIEW_EMPLOYEES"
-                },
-                // Employees by Department
-                {
-                    name: "View Employees by department",
-                    value: "Employees_Department"
-                },
-                // Employees by Manager
-                {
-                    name: "View employees by Manager",
-                    value: "Manager_ID"
-                },
-                //Add Employer
-                {
-                    name: "Select here to Add an Employee",
-                    value: "Add_Employee"
-                },
-                //Delete Employer
-                {
-                    name: "Select here to Delete an Employer",
-                    value: "Destroy_Employer"
-                },
-                //View all roles
-                {
-                    name: "View all roles",
-                    value: "View_All"
-                },
-                //More choices are needed!
-                {
-                    name: "Add more choices?",
-                    value: "Add_More"
-                },
-                // Terminate the program
-                {
-                    name: "Quit",
-                    value: "QUIT"
-                },
-            ]
-        }
-    ]).then(res => {
-        let choice = res.choices;
-    
-    //})
-};
-
+                    type: 'list',
+                    name: 'options',
+                    message: 'What would you like to do?',
+                    choices: [
+                            "View all Employees", 
+                            "View Employees by department", 
+                            "View employees by Manager", 
+                            "Select here to Add and Employee", 
+                            "View all roles", 
+                            "Add more choices", 
+                            "Quit",
+                        ],
+                    },
+                ])
+                .then((answer => tableView(answer)))
+};    
+                
 mainMenu();
+
